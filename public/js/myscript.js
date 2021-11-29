de25f0d4f5279a59b6c26832815ad00ab1a5c0a8
nandawahyubi <nandawahyubi6121@gmail.com>
2021-11-29 11:39:22 +0700

    update assets

---
+++ public/js/myscript.js
@@ -0,0 +1,65 @@
+
+function formatDate(date) {
+    var d = new Date(date),
+        year  = d.getFullYear(),
+        month = '' + (d.getMonth() + 1),
+        day   = '' + d.getDate();
+
+    if (month.length < 2) 
+        month = '0' + month;
+    if (day.length < 2) 
+        day = '0' + day;
+
+    return [year, month, day].join('-');
+}
+
+function realtimeDate(){
+    var date = new Date(document.getElementById("order-schedule").value);
+
+    /*
+      0 => Sunday
+      1 => Monday
+      2 => Tuesday
+      3 => Wednesday
+      4 => Thursday
+      5 => Friday
+      6 => Saturday
+     */
+    
+    if(date.getDay() == 4) {
+        date.setDate(date.getDate() + 4);
+    }else if(date.getDay() == 5) {
+        date.setDate(date.getDate() + 4);
+    }else if(date.getDay() == 6) {
+        date.setDate(date.getDate() + 4);
+    }else {
+        date.setDate(date.getDate() + 3);
+    }
+
+    document.getElementById("order-schedule-complate").value = formatDate(date);
+}
+
+$(function() {
+    $( "#order-schedule" ).datepicker({
+        dateFormat: "yy-mm-dd",
+        minDate: new Date(), //disable past date
+        // showOtherMonths: true,
+        // selectOtherMonths: true,
+        // beforeShowDay: function(date) { //disable sunday and saturday
+        //     var day = date.getDay();
+        //     if (day == 0 || day == 6) {
+        //       return [false];
+        //     } else {
+        //       return [true];
+        //     }
+        // }
+        beforeShowDay: function(date) { //disable sunday
+            var day = date.getDay();
+            if (day == 0) {
+              return [false];
+            } else {
+              return [true];
+            }
+          }
+    });
+});
